f198d58e4c95926a974b78a8b7bd6853c5f22b1e
Mihail Teoharidis <teomihsav76@gmail.com>
2021-03-04 13:17:57 +0200

    rest

---
+++ rest-api/index.js
@@ -0,0 +1,16 @@
+
+
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Working')
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000')
+})
+// app.listen(5000, console.log.bind(console, 'Server is listening on port 5000'))
+
+
